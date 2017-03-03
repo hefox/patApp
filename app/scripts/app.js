75acb8ca3de98ecb66a414f9fdd8a642fae9ae77
Hunter Fox <hefoxed@gmail.com>
2017-03-02 17:18:12 -0800

    Get basic app functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 /**
+
+
+/**
  * Main app code.
  */
 function PatApp(selectorId) {
@@ -9,20 +12,21 @@ function PatApp(selectorId) {
   if (!this.element) {
     throw 'Unable to find element with ID ' + this.selectorId;
   }
+  this.imageService = new ImageService();
+  this.modal = new ImageModal(this.element);
+  this.addImages();
 }
 
 /**
- * Perform app setup.
+ * Fetch and add images to dom.
  */
-PatApp.prototype.init = function() {
-  this.imageService = new ImageService();
-  this.addImages();
-};
-
 PatApp.prototype.addImages = function() {
   var that = this;
   this.images = this.imageService.getImages('cats').then(function(images) {
-    for (var key in images) {
+    for (var key = 0; key < images.length; key++) {
+      // Store references to prev/next images.
+      images[key].prev = images[key - 1] || null;
+      images[key].next = images[key + 1] || null;
       that.element.appendChild(that.createImageElement(images[key]));
     }
   });
@@ -30,8 +34,12 @@ PatApp.prototype.addImages = function() {
 
 /**
  * Returns a node ready for insert into dom with image tag.
+ *
+ * Could have done this with some combination of innerHTML, choice this
+ * method for ease of adding onclick. Prefer using some type of templates.
  */
 PatApp.prototype.createImageElement = function(image) {
+  var that = this;
   var child = document.createElement('div');
   child.className = 'photo-gallery__image-wrapper';
   // Lots of choices for preview, set it to var to change easily.
@@ -40,24 +48,26 @@ PatApp.prototype.createImageElement = function(image) {
   child.appendChild(a);
   a.className = 'photo-gallery__image-link';
   a.href = image['bitly_url'];
+  a.onclick = function(event) {
+    event.preventDefault();
+    that.modal.displayImage(image);
+    return false;
+  };
   var img = document.createElement('img');
   a.appendChild(img);
   img.width = preview.width;
   img.height = preview.height;
   img.src = preview.url;
   img.className = 'photo-gallery__image';
-
   return child;
 };
-
 
 // Well supported way to add an on document ready event.
 document.addEventListener('DOMContentLoaded', function() {
   try {
-    var app = new PatApp('pat-photo-gallery');
-    app.init();
+    new PatApp('pat-photo-gallery');
   }
   catch (e) {
-   window.alert.log(e.message); // pass exception object to err handler
+   window.alert(e.message); // pass exception object to err handler
   }
 });
---
+++ app/scripts/modal.js
@@ -0,0 +1,110 @@
+'use strict';
+
+/*exported ImageModal */
+
+/**
+ * Modal code
+ */
+function ImageModal(parentElement) {
+  this.parentElement = parentElement;
+  this.element = this.createModalElement();
+  this.parentElement.appendChild(this.element);
+  this.elementClass = 'photo-gallery__modal-wrapper';
+}
+
+/**
+ * Create modal.
+ */
+ImageModal.prototype.displayImage = function(image) {
+  var that = this;
+  var caption = this.element.getElementsByClassName('photo-gallery__modal-image-caption')[0];
+  caption.innerHTML = image.slug || 'No Caption';
+  var wrapper = this.element.getElementsByClassName('photo-gallery__modal-image-wrapper')[0];
+  var img = document.createElement('img');
+  var imageToDisplay = image.images['original'];
+  img.src = imageToDisplay.url;
+  img.width = imageToDisplay.width;
+  img.height = imageToDisplay.height;
+  img.className = 'photo-gallery__modal-image';
+  // Add or replace image.
+  if (this.img) {
+    wrapper.replaceChild(img, this.img);
+  }
+  else {
+    wrapper.insertBefore(img, caption);
+  }
+  this.img = img;
+  this.element.style.display = 'block';
+  var prev = this.element.getElementsByClassName('photo-gallery__modal-control--prev')[0];
+  var next = this.element.getElementsByClassName('photo-gallery__modal-control--next')[0];
+  console.log(image);
+  if (image.prev) {
+    prev.style.display = 'inline';
+    prev.onclick = function(event) {
+      event.preventDefault();
+      that.displayImage(image.prev);
+      return false;
+    };
+  }
+  else {
+    prev.style.display = 'none';
+  }
+  if (image.next) {
+    next.style.display = 'inline';
+    next.onclick = function(event) {
+      event.preventDefault();
+      that.displayImage(image.next);
+      return false;
+    };
+  }
+  else {
+    next.style.display = 'none';
+  }
+};
+
+/**
+ * Create modal.
+ */
+ImageModal.prototype.closeModal = function() {
+  this.element.style.display = 'none';
+};
+
+/**
+ * Create modal.
+ */
+ImageModal.prototype.createModalElement = function() {
+  var that = this;
+  var child = document.createElement('div');
+  child.style.display = 'none';
+  child.className = 'photo-gallery__modal-wrapper';
+  child.innerHTML = '<div class="photo-gallery__modal-controls">' +
+      '<a href="#" class="photo-gallery__modal-control photo-gallery__modal-control--prev" title="Previous Image">&#60; </a> ' +
+      '<a href="#" class="photo-gallery__modal-control photo-gallery__modal-control--next" title="Next Image">&#62; </a> ' +
+      '<a href="#" class="photo-gallery__modal-control photo-gallery__modal-control--close" title="Close Image"> × </a>' +
+    '</div>' +
+    '<div class="photo-gallery__modal-content">' +
+      '<div class="photo-gallery__modal-image-wrapper">' +
+        '<div class="photo-gallery__modal-image-caption"></div>' +
+      '</div>' +
+    '</div>';
+  var span = child.getElementsByClassName('photo-gallery__modal-control--close')[0];
+  span.onclick = function(event) {
+    event.preventDefault();
+    that.closeModal();
+    return false;
+  };
+  // Page modal on arrow clicks.
+  // Triggering the next/prev to not replicate logic.
+  document.onkeydown = function(event) {
+    switch(event.which || event.keyCode) {
+        case 37: // left arrow key
+        child.getElementsByClassName('photo-gallery__modal-control--prev')[0].click();
+        break;
+
+        case 39: // right arrow key
+        child.getElementsByClassName('photo-gallery__modal-control--next')[0].click();
+        break;
+    }
+  };
+  return child;
+};
